37c944fe6da307c0611b4c93c7c81cd77aa8e57d
“Sergei <serg.galchin@gmail.com>
2024-01-09 00:18:14 +0200

    Added auth_user_model and auth_token

--- orders/orders/settings.py
+++ orders/orders/settings.py
@@ -40,6 +40,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'rest_framework',
+    'rest_framework.authtoken',
+    'django_rest_passwordreset',
+
     'backend',
 ]
 
@@ -129,3 +133,11 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'backend.User'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+    ),
+}
