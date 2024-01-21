4fb609fbc97614cf7d1504dba284408dbc716019
“Sergei <serg.galchin@gmail.com>
2024-01-21 16:18:19 +0200

    work with user add

--- orders/backend/urls.py
+++ orders/backend/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
-from .views import UserRegister, EmailConfirm
+from django_rest_passwordreset.views import reset_password_request_token, reset_password_confirm
+from .views import UserRegister, EmailConfirm, UserLogin
 
 app_name = 'backend'
 
 urlpatterns = [
     path('user/register', UserRegister.as_view(), name='user-register'),
     path('user/register/confirm', EmailConfirm.as_view(), name='email-confirm'),
+    path('user/login', UserLogin.as_view(), name='user-login'),
+    path('user/password_reset', reset_password_request_token, name='password-reset'),
+    path('user/password_reset/confirm', reset_password_confirm, name='password-reset-confirm'),
 ]
